8af3463ceece1fc8eafe86c4477c25b02e52b4e0
irfanshks97 <irfanshks97@gmail.com>
2024-07-30 08:09:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AnologClock from "./Components/AnologClock/AnologClock";
+import { Clocksecond } from "./Components/ClockSecond/Clocksecond";
+
+function App() {
+  return (
+    <>
+      <Clocksecond />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/AnologClock/AnologClock.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Clocksecond = () => {
+  return (
+    <>
+      <div className="clock"></div>
+    </>
+  );
+};
